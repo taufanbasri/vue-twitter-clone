aaabd5848117e8ef49df60e8edd0c88889baef17
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-07 22:20:54 +0700

    Left Sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
+<script setup>
+const darkMode = ref(false)
+</script>
+
 <template>
-  <div>
-    <h1 class="text-red-500">Twitter Clone</h1>
+  <div :class="{'dark': darkMode}">
+    <div class="min-h-full">
+      <div class="bg-white dark:bg-dim-900">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- Left Sidebar -->
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarsLeft />
+            </div>
+          </div>
+          <!-- End Left Sidebar -->
+
+          <!-- Main Content -->
+
+
+          <!-- Right Sidebar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
---
+++ components/sidebars/left/index.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { HomeIcon } from "@heroicons/vue/24/solid";
+import { HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, DocumentTextIcon, UserIcon, EllipsisHorizontalCircleIcon } from "@heroicons/vue/24/outline";
+
+const { defaultTransition } = useTailwindConfig()
+</script>
+
+<template>
+  <div class="h-screen flex flex-col">
+    <div class="p-2 my-2 rounded-full w-min dark:hover:bg-white/20 hover:bg-blue-50" :class="defaultTransition">
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+
+    <div class="mt-2 space-y-3">
+      <SidebarsLeftTab active>
+        <template v-slot:icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name>
+          Home
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <HashtagIcon />
+        </template>
+        <template v-slot:name>
+          Explore
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <BellIcon />
+        </template>
+        <template v-slot:name>
+          Notifications
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <InboxIcon />
+        </template>
+        <template v-slot:name>
+          Messages
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <BookmarkIcon />
+        </template>
+        <template v-slot:name>
+          Bookmark
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <DocumentTextIcon />
+        </template>
+        <template v-slot:name>
+          Lists
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name>
+          Profile
+        </template>
+      </SidebarsLeftTab>
+
+      <SidebarsLeftTab>
+        <template v-slot:icon>
+          <EllipsisHorizontalCircleIcon />
+        </template>
+        <template v-slot:name>
+          More
+        </template>
+      </SidebarsLeftTab>
+    </div>
+  </div>
+</template>
